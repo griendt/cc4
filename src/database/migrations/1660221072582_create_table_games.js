50e32f26a2444ce45a053d67002f7413dbc8b694
griendt <alex@e-cube.nl>
2022-08-12 15:45:10 +0200

    Add display_name and description to games table

--- src/database/migrations/1660221072582_create_table_games.js
+++ src/database/migrations/1660221072582_create_table_games.js
@@ -5,6 +5,8 @@ module.exports = {
             created_at timestamp null,
             updated_at timestamp null,
             deleted_at timestamp null,
+            display_name varchar(255) not null,
+            description varchar(255) null,
             started_at timestamp null,
             ended_at timestamp null,
             \`blob\` json null
